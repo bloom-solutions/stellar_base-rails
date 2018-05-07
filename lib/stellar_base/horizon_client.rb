c31009455ab4bd90691183afb6010997b97b0d7a
Ace Subido <ace.subido@gmail.com>
2018-05-07 16:14:32 +0800

    Add a 'HorizonClient#get_transaction' method to fetch transactions by id

--- lib/stellar_base/horizon_client.rb
+++ lib/stellar_base/horizon_client.rb
@@ -1,14 +1,24 @@
 module StellarBase
   class HorizonClient
     def get_operation(operation_id)
-      response = HTTParty.get(base_operations_path(operation_id))
+      response = HTTParty.get(horizon_operations_path(operation_id))
+
+      JSON.parse(response.body) if response.code == 200
+    end
+
+    def get_transaction(transaction_id)
+      response = HTTParty.get(horizon_transactions_path(transaction_id))
 
       JSON.parse(response.body) if response.code == 200
     end
 
     private
 
-    def base_operations_path(operation_id)
+    def horizon_transactions_path(transaction_id)
+      "#{base_url}/transactions/#{transaction_id}"
+    end
+
+    def horizon_operations_path(operation_id)
       "#{base_url}/operations/#{operation_id}"
     end
 
