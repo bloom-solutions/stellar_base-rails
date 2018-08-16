8bc1a0be3cb8b6be45e633acbbbb3c1c971b07dc
Ramon Tayag <ramon.tayag@gmail.com>
2018-08-16 14:40:08 +0800

    Reset the gem configuration each run
    This allows for more consistency in specs

--- spec/support/config.rb
+++ spec/support/config.rb
@@ -1,12 +1,15 @@
-StellarBase.configure do |c|
-  c.modules = %i(bridge_callbacks)
-  c.horizon_url = "https://horizon-testnet.stellar.org"
+RSpec.configure do |c|
 
-  c.on_bridge_callback = "ProcessBridgeCallback"
-  c.check_bridge_callbacks_authenticity = false
-  c.check_bridge_callbacks_mac_payload = false
-  c.bridge_callbacks_mac_key = "sample"
+  c.before(:each) do
+    StellarBase.configure do |c|
+      c.modules = %i(bridge_callbacks)
+      c.horizon_url = "https://horizon-testnet.stellar.org"
+
+      c.on_bridge_callback = "ProcessBridgeCallback"
+      c.check_bridge_callbacks_authenticity = false
+      c.check_bridge_callbacks_mac_payload = false
+      c.bridge_callbacks_mac_key = "sample"
+    end
+  end
+
 end
-
-
-
