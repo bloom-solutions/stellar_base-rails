b67df47b6607620ffeb009867fe77b6205591024
Mark Chavez <markchav3z@gmail.com>
2018-08-16 09:53:35 +0800

    Add /withdraw endpoint if module is included

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ StellarBase::Engine.routes.draw do
   if StellarBase.included_module?(:bridge_callbacks)
     resources :bridge_callbacks, only: [:create], defaults: { format: "json" }
   end
+
+  if StellarBase.included_module?(:withdraw)
+    resource :withdraw, only: [:show], defaults: { format: "json" }
+  end
 end
 
