79fdab042c33e543311d9f9b5c38007d0ade4972
Ace Subido <ace.subido@gmail.com>
2018-10-01 15:58:33 +0800

    Create Deposit model

---
+++ db/migrate/20181001070647_create_stellar_base_deposits.rb
@@ -0,0 +1,18 @@
+class CreateStellarBaseDeposits < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :stellar_base_deposits do |t|
+      t.integer :deposit_request_id, index: true
+      t.string :tx_id, index: true
+      t.decimal :amount
+      t.string :stellar_operation_id
+
+      t.timestamps
+    end
+
+    add_foreign_key(:stellar_base_deposits, :stellar_base_deposit_requests, {
+      column: :deposit_request_id,
+    })
+  end
+
+end
