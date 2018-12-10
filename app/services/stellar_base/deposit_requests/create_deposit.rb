56bfe37c4778e7688124d096aafcf6f8b975bb5d
Ace Subido <ace.subido@gmail.com>
2018-12-10 12:38:57 +0800

    Fail CreateDeposit if a Deposit has been found

--- app/services/stellar_base/deposit_requests/create_deposit.rb
+++ app/services/stellar_base/deposit_requests/create_deposit.rb
@@ -8,19 +8,24 @@ module StellarBase
       promises :deposit
 
       executed do |c|
-        c.deposit = Deposit.find_or_create_by(
-          deposit_request_id: c.deposit_request.id,
-          tx_id: c.tx_id,
-        ) do |deposit|
-          deposit.amount = c.amount
+        deposit_request_id = c.deposit_request.id
+        tx_id = c.tx_id
+
+        deposit = Deposit.find_by(
+          deposit_request_id: deposit_request_id,
+          tx_id: tx_id,
+        )
+
+        if deposit.present?
+          c.deposit = deposit
+          c.fail_and_return! "Deposit already created for tx #{tx_id}"
         end
 
-        stellar_tx_id = c.deposit.stellar_tx_id
-
-        if stellar_tx_id.present?
-          c.skip_remaining! "Deposit previously made: stellar_tx_id " \
-            "#{stellar_tx_id}, skipping"
-        end
+        c.deposit = Deposit.create(
+          deposit_request_id: deposit_request_id,
+          tx_id: tx_id,
+          amount: c.amount,
+        )
       end
 
     end
