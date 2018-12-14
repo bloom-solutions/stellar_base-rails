cfbb6a44ca508fb5fd82619aa4da34909288d7c7
Ace Subido <ace.subido@gmail.com>
2018-12-14 22:01:46 +0800

    Add defaults for StellarOperation and StellarTransaction factories

--- lib/stellar_base/factories/stellar_transactions.rb
+++ lib/stellar_base/factories/stellar_transactions.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   factory :stellar_base_stellar_transaction, class: "StellarBase::StellarTransaction" do
-    id
-    hash
-    paging_token
-    memo
-    memo_type
-    created_at
-    source_account
-    source_account_sequence
-    ledger
-    fee_paid
-    operation_count
+    id "1234"
+    hash "1234"
+    paging_token "1234"
+    memo ""
+    memo_type "none"
+    created_at { Time.zone.now }
+    source_account "G-SOURCE"
+    source_account_sequence "1234"
+    ledger "1098"
+    fee_paid 100
+    operation_count 1
 
     skip_create
   end
