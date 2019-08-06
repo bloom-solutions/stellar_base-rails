d8cb55640db0a5d8df2e9882167ec811927fc2a5
Ace Subido <ace.subido@gmail.com>
2019-08-06 17:44:08 +0800

    Create FeeRequests::Operations::Show

---
+++ app/concepts/stellar_base/fee_requests/operations/show.rb
@@ -0,0 +1,42 @@
+module StellarBase
+  module FeeRequests
+    module Operations
+      class Show < ApplicationOperation
+
+        POLICY_ERROR_MESSAGE = "You are unauthorized to request fee details".freeze
+
+        step self::Policy::Pundit(FeeRequestPolicy, :show?)
+        failure :set_policy_error!
+        step Model(FeeRequest, :new)
+        step Contract::Build(constant: Contracts::Show)
+        step Contract::Validate(key: :fee_request)
+        step Contract::Persist(method: :sync)
+        step :find_asset_details!
+        success :set_fee_response!
+
+        private
+
+        def set_policy_error!(options, params:, **)
+          options["result.policy.message"] = POLICY_ERROR_MESSAGE
+        end
+
+        def find_asset_details!(options, params:, **)
+          details = FindAssetDetails.(
+            operation: params[:fee_request][:operation],
+            asset_code: params[:fee_request][:asset_code],
+          )
+          options[:asset_details] = details.presence || {}
+        end
+
+        def set_fee_response!(options, model:, asset_details:, params:, **)
+          model.fee_response = FeeRequests::CallFeeFrom.(
+            fee_request: model,
+            fee_from: asset_details[:fee_from],
+          )
+        end
+
+      end
+    end
+  end
+end
+
---
+++ spec/concepts/stellar_base/fee_requests/operations/show_spec.rb
@@ -0,0 +1,68 @@
+require "spec_helper"
+
+module StellarBase
+  module FeeRequests
+    module Operations
+      RSpec.describe Show do
+
+        it "Can request withdraw fees" do
+          op = described_class.(fee_request: {
+            asset_code: "BTCT",
+            operation: "withdraw",
+            amount: 0.01,
+          })
+
+          expect(op).to be_success
+          fee_request = op["model"]
+          expect(fee_request.asset_code).to eq "BTCT"
+          expect(fee_request.operation).to eq "withdraw"
+          expect(fee_request.amount).to eq 0.01
+          expect(fee_request.type).to be_nil
+
+          # from GetWithdrawFeeFrom
+          expect(fee_request.fee_response).to eq 0.0001
+        end
+
+        it "Can request deposit fees" do
+          op = described_class.(fee_request: {
+            asset_code: "BTCT",
+            operation: "deposit",
+            amount: 0.01,
+          })
+
+          expect(op).to be_success
+          fee_request = op["model"]
+          expect(fee_request.asset_code).to eq "BTCT"
+          expect(fee_request.operation).to eq "deposit"
+          expect(fee_request.amount).to eq 0.01
+          expect(fee_request.type).to be_nil
+
+          # fee_from is not configured for depositable_assets -> BTCT
+          expect(fee_request.fee_response).to be_zero
+        end
+
+        context "module is not loaded" do
+          before do
+            StellarBase.configure do |c|
+              c.modules = %w[fees]
+            end
+          end
+
+          it "returns a policy error message" do
+            op = described_class.(withdrawal_request: {
+              asset_code: "BTCT",
+              operation: "withdraw",
+              amount: 0.01,
+            })
+
+            expect(op).to_not be_success
+            expect(op["result.policy.default"]).to_not be_success
+            expect(op["result.policy.message"])
+              .to eq described_class::POLICY_ERROR_MESSAGE
+          end
+        end
+      end
+
+    end
+  end
+end
